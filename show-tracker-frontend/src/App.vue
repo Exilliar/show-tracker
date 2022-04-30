559dd65b3e12faf7ace3c96d8fcbf6ad7dbc65de
exilliar <rorywebber1000@gmail.com>
2022-04-30 18:38:33 +0100

    made basic frontend

---
+++ show-tracker-frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let count = ref(0);
+</script>
+
+<template>
+  <p>This is working!</p>
+  <p>Count: {{ count }}</p>
+  <button @click="count++">inc count</button>
+</template>
+
+<style>
+
+</style>
